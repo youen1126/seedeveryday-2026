b75b59d9d55efe909e80691a05baae76641e94b7
UN <youen1126@gmail.com>
2026-03-20 16:14:14 +0800

    fix：漢堡選單bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,6 +18,20 @@ const Header = () => {
     <div className="bg-white sticky-top">
       <div className="container">
         <nav className="navbar px-0 navbar-expand-lg navbar-light bg-white">
+          {/* 左：漢堡 */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* 中：logo */}
           <NavLink
             className="navbar-brand position-absolute"
             to="/"
@@ -34,18 +48,18 @@ const Header = () => {
               }}
             />
           </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          {/* 右：購物車（關鍵！） */}
+          <div className="position-absolute end-0 me-3">
+            <NavLink to="/cart">
+              <div className="position-relative">
+                <i className="fas fa-shopping-cart"></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  {carts.length}
+                </span>
+              </div>
+            </NavLink>
+          </div>
+          {/* collapse（獨立一層） */}
           <div
             className="collapse navbar-collapse bg-white custom-header-md-open"
             id="navbarNav"
@@ -64,16 +78,6 @@ const Header = () => {
                 </li>
               </ul>
             </div>
-          </div>
-          <div className="d-flex">
-            <NavLink to="/cart">
-              <div className="position-relative">
-                <i className="fas fa-shopping-cart"></i>
-                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                  {carts.length}
-                </span>
-              </div>
-            </NavLink>
           </div>
         </nav>
       </div>
