8ba480f7e61fe5b23a3990d1e4d3404f73972565
UN <youen1126@gmail.com>
2026-03-20 16:37:27 +0800

    fix：優化

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import { Provider } from "react-redux";
 import { store } from "../store.js";
 
 createRoot(document.getElementById("root")).render(
-  //<StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  //</StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
 );
