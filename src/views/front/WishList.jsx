1e5bcea3a38819e75d4281405432e0d064967470
UN <youen1126@gmail.com>
2026-03-23 17:32:25 +0800

    feat:增加加入購物車按鈕

--- src/views/front/WishList.jsx
+++ src/views/front/WishList.jsx
@@ -1,9 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { toggleWishlistItem } from "../../slice/wishlistSlice";
 import { Link } from "react-router";
+import { createAsyncAddCart } from "../../slice/cartSlice";
+import useMessage from "../../hooks/useMessage";
 
 export default function WishList() {
   const dispatch = useDispatch();
+  const { showSuccess } = useMessage();
 
   // wishlist 狀態
   const wishList = useSelector((state) => state.wishlist.items);
@@ -13,6 +16,18 @@ export default function WishList() {
 
   // 篩選出「有收藏的商品」
   const wishListItems = products.filter((item) => wishList[item.id]);
+
+  //將商品加入購物車
+  const handleAddCart = (e, id, qty = 1) => {
+    e.preventDefault();
+    dispatch(
+      createAsyncAddCart({
+        id,
+        qty,
+      }),
+    );
+    showSuccess("成功加入購物車");
+  };
 
   return (
     <div className="container py-5">
@@ -71,6 +86,15 @@ export default function WishList() {
               <div className="card-body">
                 <h5 className="card-title">{item.title}</h5>
                 <p className="card-text">NT$ {item.price}</p>
+                <div className="py-3">
+                  <button
+                    type="button"
+                    className="text-nowrap btn btn-dark w-20 p-2"
+                    onClick={(e) => handleAddCart(e, item.id)}
+                  >
+                    加入購物車
+                  </button>
+                </div>
               </div>
             </div>
           </div>
