4a63023db6b113c7fe6aee3ff45d0ec5897801c6
UN <youen1126@gmail.com>
2026-03-23 16:48:39 +0800

    fix：修footer貼底

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -4,12 +4,12 @@ import { Outlet } from "react-router";
 
 export default function FrontendLayout() {
   return (
-    <>
+    <div className="app-wrapper">
       <Header />
       <main className="main-content">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
